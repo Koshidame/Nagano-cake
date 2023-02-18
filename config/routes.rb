6885aa5a403d1912dedf3057698e7c9631b061f6
EC2 Default User <ec2-user@ip-172-31-24-162.ap-northeast-1.compute.internal>
2023-02-18 05:50:17 +0000

    [add] ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,37 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'makings/update'
+  # namespace :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+  # end
+  
+  namespace :public do
+    resources :items, only: [:index, :show]
   end
   
-  namespace :admin do
-    resources :orders, only: [:edit, :update]
+  namespace :public do
+    resource :customers, only: [:show, :edit, :update]
+    get '/orders/unsubscribe' => 'customers#unsubscribe'
+    patch '/customers/withdrawal' => 'customers#withdrawal'
   end
   
-  namespace :admin do
-    resources :customers, only: [:index, :show, :edit, :update]
+  namespace :public do
+    resources :cart_items, only: [:index, :update, :update, :destroy, :create]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all' 
   end
   
+  namespace :public do
+    resources :orders, only:[:new, :create, :index, :show]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/complete' => 'orders#complete'
+  end
+  
+  namespace :public do
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  
+  
   namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update]
+    get '/' => 'homes#top'
   end
   
   namespace :admin do
@@ -20,32 +39,19 @@ Rails.application.routes.draw do
   end
   
   namespace :admin do
-    get 'homes/top'
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   
-  namespace :public do
-    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   
-  namespace :public do
-    resources :orders, only:[:new, :confirm, :complete, :create, :index, :show]
+  namespace :admin do
+    resources :orders, only: [:show, :update]
   end
   
-  namespace :public do
-    resources :cart_items, only: [:index, :update, :update, :destroy, :destroy, :create]
-  end
-  
-  namespace :public do
-    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdrawal]
-  end
-  
-  namespace :public do
-    resources :items, only: [:index, :show]
-  end
-  
-  namespace :public do
-    resources :homes, only: [:top, :about]
-
+  namespace :admin do
+    resources :order_details, only: [:update]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
