7411313ad828702f773122c5331ec37cf820c7dc
EC2 Default User <ec2-user@ip-172-31-24-162.ap-northeast-1.compute.internal>
2023-02-18 10:08:40 +0000

    [add] routes まとめた

--- config/routes.rb
+++ config/routes.rb
@@ -9,57 +9,26 @@ Rails.application.routes.draw do
 }
 
   namespace :public do
-    root to: 'homes#top'
+    get '/' => 'homes#top'
     get '/about' => 'homes#about'
-  end
-
-  namespace :public do
     resources :items, only: [:index, :show]
-  end
-
-  namespace :public do
     resource :customers, only: [:show, :edit, :update]
     get '/orders/unsubscribe' => 'customers#unsubscribe'
     patch '/customers/withdrawal' => 'customers#withdrawal'
-  end
-
-  namespace :public do
     resources :cart_items, only: [:index, :update, :update, :destroy, :create]
     delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
-  end
-
-  namespace :public do
     resources :orders, only:[:new, :create, :index, :show]
     post '/orders/confirm' => 'orders#confirm'
     get '/orders/complete' => 'orders#complete'
-  end
-
-  namespace :public do
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
 
-
   namespace :admin do
     get '/' => 'homes#top'
-  end
-
-  namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-  end
-
-  namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
-  end
-
-  namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
-  end
-
-  namespace :admin do
     resources :orders, only: [:show, :update]
-  end
-
-  namespace :admin do
     resources :order_details, only: [:update]
   end
 
