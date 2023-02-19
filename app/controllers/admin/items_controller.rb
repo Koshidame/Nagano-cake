a344c3ea05be6aba4b19cff514f543aad391f954
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-02-19 04:12:40 +0000

    [add]items保存機能追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_product, only: %i[show edit update]
+
   def index
   end
 
   def new
     @item = Item.new
-    @genres =Genre.all
+    # @genres =Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
   end
 
   def show
