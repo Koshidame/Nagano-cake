44c3cc79a14d2a41337812c38db9f0fcd7a67a8f
EC2 Default User <ec2-user@ip-172-31-24-162.ap-northeast-1.compute.internal>
2023-02-20 11:12:36 +0000

    [add] 管理者ログイン後の移動先修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
-      admin_order_path
+      admin_path
     else
-      public_path
+      root_path
     end
   end
 
--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,34 +1,16 @@
 class Public::AddressesController < ApplicationController
   def index
-    @address = Address.new
-    @addresses = Address.all
   end
 
   def edit
   end
 
   def create
-    @address = Address.new(address_params)
-    @address.customer_id = current_customer.id
-    if @address.save
-      redirect_to public_addresses_path, notice: "配送先を登録しました"
-    else
-      @addresses = Address.all
-      render :index
-    end
   end
 
   def update
   end
 
   def destroy
-    @address = Address.find(params[:id])
-    @address.destroy
-    redirect_to public_addresses_path
   end
-  
-  private
-    def address_params
-      params.require(:address).permit(:postal_code, :address, :name)
-    end
 end
