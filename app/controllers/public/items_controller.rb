1703fd17abea4bc84dad150cdc83245e4189d846
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-02-20 07:09:06 +0000

    indexとshow

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,24 @@
 class Public::ItemsController < ApplicationController
+  
   def index
-    @items = Item.all
+    @genres = Genre.where(is_active: true)
+    if params[:genre_id]
+		  @genre = Genre.find(params[:genre_id])
+		  @products = @genre.products.where(is_active: true).page(params[:page]).reverse_order
+    else
+      @products = Product.where(is_active: true).page(params[:page]).reverse_order
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+    @genres = Genre.where(is_active: true)
+    @cart_product = CartProduct.new
   end
+
+  private
+  	
+	def params_product
+		parmas.require(:product).permit(:image ,:name )
+	end
 end
