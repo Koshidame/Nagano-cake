81b8a3dd0e0acbf0edfd813548c8fa7ae60d6d2d
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-02-21 09:24:03 +0000

    中断

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -23,6 +23,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    # params.require(:order).permit(:payment_method)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
