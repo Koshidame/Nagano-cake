742d0be68d210e13d644b0c43da3540a9f6cfee1
EC2 Default User <ec2-user@ip-172-31-0-183.ap-northeast-1.compute.internal>
2023-02-22 15:23:06 +0000

    まだ改良中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,22 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
-  @orders =Order.find(params[:id])
-  
+    @orders =Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
   end
 
   def update
+     @orders = Order.find(params[:id])
+    if@orders.update(order_params)
+      redirect_to admin_order_path(@orders.id)
+    else
+      redirect_to admin_genres_path
+    end
   end
-private
-  def oreder_params
-  params.require(:order).permit(:id,:customer_id,:postal_code,:address,:name,:shipping_cost,:status,:total_payment,:payment_method)
-  end
+
+
+  private
+    def order_params
+     params.require(:order).permit(:id,:customer_id,:postal_code,:address,:name,:shipping_cost,:status,:total_payment,:payment_method)
+    end
 end
