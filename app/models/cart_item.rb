5f258e31532fa0d1b9a685e8d1e98ec88d052982
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-02-23 05:50:52 +0000

    [add]optional true

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,13 +1,13 @@
 class CartItem < ApplicationRecord
-  belongs_to :customer
-  belongs_to :item
+  belongs_to :customer, optional: true
+  belongs_to :item, optional: true
   has_one_attached :image
-  
+
   validates :item_id, presence: true
   validates :amount, numericality:{ only_integer: true }
-  
-  
-  
+
+
+
   def sub_total
     item.price * amount
   end
