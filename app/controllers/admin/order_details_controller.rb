2f1ae06d824f9cbfbf19230749118c6cf449206a
EC2 Default User <ec2-user@ip-172-31-0-183.ap-northeast-1.compute.internal>
2023-02-23 04:42:30 +0000

    あともうちょっと

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,17 @@
 class Admin::OrderDetailsController < ApplicationController
+  
   def update
+    @orders = OrderDetail.find(params[:id])
+    if @orders.update(order_detail_params)
+      redirect_to admin_order_path(@orders.order_id)
+    else
+      redirect_to admin_genres_path
+    end
   end
+
+   private
+    def order_detail_params
+     params.require(:order_detail).permit(:id,:order_id,:item_code,:price,:amount,:making_status,:updated_at)
+    end
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
+  enum making_status: { not_making: 0, wating_production: 1, production: 2,don: 3 }
 end
